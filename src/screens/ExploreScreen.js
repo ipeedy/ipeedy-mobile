e2a0be29c06f84b967195ea87e86164e7c13ca99
Quốc Khánh <bkdev98@gmail.com>
2017-09-03 16:25:42 +0700

    add mapview

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react';
 import styled from 'styled-components/native';
 import { graphql, withApollo, compose } from 'react-apollo';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
 
 import { getUserInfo } from '../actions/user';
+import MapStyle from '../utils/mapstyle';
 
 import ME_QUERY from '../graphql/queries/me';
 
 const Root = styled.View`
   flex: 1;
-  justifyContent: center;
-  alignItems: center;
   backgroundColor: ${props => props.theme.WHITE};
 `;
 
-const Title = styled.Text`
-  fontSize: 25;
-  color: ${props => props.theme.PRIMARY};
-  fontFamily: 'quicksand-bold';
+const MapContainer = styled.View`flex: 7.5;`;
+
+const Map = styled(MapView).attrs({
+  provider: MapView.PROVIDER_GOOGLE,
+  customMapStyle: MapStyle,
+})`
+  flex: 1;
+`;
+
+const ProductContainer = styled.View`
+  flex: 2.5;
+  backgroundColor: ${props => props.theme.WHITE};
 `;
 
 class ExploreScreen extends Component {
@@ -33,7 +41,17 @@ class ExploreScreen extends Component {
   render() {
     return (
       <Root>
-        <Title>EXPLORE SCREEN</Title>
+        <MapContainer>
+          <Map
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </MapContainer>
+        <ProductContainer />
       </Root>
     );
   }
